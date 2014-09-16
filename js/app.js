3fbdfcc6ad44cd419eda6b04899f332d426a26a8
Paul Hermany <paulhermany@gmail.com>
2014-09-16 11:17:34 -0400

    Changed requireJS to use local fallback for CDN resources

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 requirejs.config({
     paths: {
-		//https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
-        "jquery": "lib/jquery-1.11.1.min",
-		//https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js
-        "bootstrap": "lib/bootstrap-3.2.0.min"
+		jquery : [
+			'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
+			'lib/jquery-1.11.1.min'
+		],
+		bootstrap : [
+			'https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js',
+			'lib/bootstrap-3.2.0.min'
+		]
     },
     shim: {
-        "bootstrap": {
-            deps: ["jquery"]
+        bootstrap : {
+            deps: ['jquery']
         }
     }
 });
 
-require(["jquery", "bootstrap"], function ($) {
+require(['jquery', 'bootstrap'], function ($) {
     // do nothing
 });
