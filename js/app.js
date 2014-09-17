d8274d5b4cfe2e73705d823c9c4fa01b02366d96
Paul Hermany <paulhermany@gmail.com>
2014-09-17 09:48:31 -0400

    Fixed trailing .js extension on RequireJS urls

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 requirejs.config({
     paths: {
 		jquery : [
-			'//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
+			'//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min',
 			'lib/jquery-1.11.1.min'
 		],
 		bootstrap : [
-			'//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js',
+			'//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min',
 			'lib/bootstrap-3.2.0.min'
 		]
     },
