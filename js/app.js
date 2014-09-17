34a08435ba63a0b47c6af8d40dac424a70048e27
Paul Hermany <paulhermany@gmail.com>
2014-09-17 09:44:40 -0400

    Made CDN references protocol-agnostic

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 requirejs.config({
     paths: {
 		jquery : [
-			'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
+			'//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
 			'lib/jquery-1.11.1.min'
 		],
 		bootstrap : [
-			'https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js',
+			'//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js',
 			'lib/bootstrap-3.2.0.min'
 		]
     },
